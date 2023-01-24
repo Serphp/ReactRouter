1d48d922e8ed25ced3bcfd991b26cf5d66edd50e
Bryan Rodriguez <Serphp@hotmail.com>
2023-01-23 22:38:23 -0500

    [Final] Default Auth with FireBase

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,43 @@
-import '../styles/Home.css'
-import React from 'react'
+// import '../styles/Home.css'
+// import React from 'react'
 
-export default function Home() {
-return (
-    <main>
-    <div className='home-cc'>
-    <div className='text-cc'>
-        <h1>DISCOVER YOUR NEXT AMIIBO</h1>
-        <div className='description-container'>
-            <p>Disponible en las principales tiendas de aplicaciones digitales</p>
-            <button className='purple-btn'>SEE THE CATALOGUE</button>
-        </div>
-    </div>
-        <img className='gba' src="/img/gba.png" alt="" />
-</div>
-</main>
-)
-}
+// export default function Home() {
+// return (
+//     <main>
+//     <div className='home-cc'>
+//     <div className='text-cc'>
+//         <h1>DISCOVER YOUR NEXT AMIIBO</h1>
+//         <div className='description-container'>
+//             <p>Disponible en las principales tiendas de aplicaciones digitales</p>
+//             <button className='purple-btn'>SEE THE CATALOGUE</button>
+//         </div>
+//     </div>
+//         <img className='gba' src="/img/gba.png" alt="" />
+// </div>
+// </main>
+// )
+// }
 
 
-// const Home = () => {
-//     return (
-//         <>
-//         <hgroup>
-//         <h2> Home {import.meta.env.API_APP_SERPHP}</h2>
-//         <h3> Inicio de la pagina </h3>
-//         </hgroup>
+const Home = () => {
+    return (
+        <>
+        <hgroup>
+        <h2> Home {import.meta.env.API_APP_SERPHP}</h2>
+        <h3> Inicio de la pagina </h3>
+        </hgroup>
 
-//         <div className="img">
-//             <img className="rd" src="https://i.imgur.com/jM3Nvp8.jpeg" alt="" />
-//             </div>
-//         <blockquote className="cc">
-//         "Un origami que encontre por ahi, blanco como papel."
-//         <footer>
-//         <cite>- Bryan</cite>
-//         </footer>
-//         </blockquote>
-//         </>
-//     )
-// };
+        <div className="img">
+            <img className="rd" src="https://i.imgur.com/jM3Nvp8.jpeg" alt="" />
+            </div>
+        <blockquote className="cc">
+        "Un origami que encontre por ahi, blanco como papel."
+        <footer>
+        <cite>- Bryan</cite>
+        </footer>
+        </blockquote>
+        </>
+    )
+};
 
-// export default Home;
+export default Home;
